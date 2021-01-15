30bcfac4e0a259cfb7346170bb9d85254665f7f5
dav-par <david.park86@gmail.com>
2021-01-15 17:41:28 +0000

    Day2 homework

---
+++ Candy/script.js
@@ -0,0 +1,18 @@
+/* 
+
+1 blue, 2 Red, 3 green, 4 yellow, 5 pink
+1 2 3 4 5 1 2 3
+4 5 1 2 3 4 5 1
+2 3 4 5 1 2 3 4
+5 1 2 3 4 5 1 2
+3 4 5 1 2 3 4 5
+1 2 3 4 5 1 2 3
+4 5 1 2 3 4 5 1
+2 3 4 5 1 2 3 4 */
+
+const colors = ['blue', 'red', 'green', 'yellow', 'pink'];
+var boxes = document.getElementsByClassName('box');
+
+for(var i=0; i<boxes.length; i++){
+    boxes[i].style.backgroundColor = colors[parseInt(Math.random()*5)];
+}
