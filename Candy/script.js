f028d2b4cb11c6106dc08d06a5d2841a6f4a7e5b
dav-par <david.park86@gmail.com>
2021-01-16 14:44:23 +0000

    Mid lessoon

--- Candy/script.js
+++ Candy/script.js
@@ -10,9 +10,80 @@
 4 5 1 2 3 4 5 1
 2 3 4 5 1 2 3 4 */
 
+const GRID_SIZE = 8;
+const GRID_COUNT = GRID_SIZE*GRID_SIZE;
+
+const CLASES = ['c1', 'c2', 'c3', 'c4', 'c5'];
+const NO_OF_BOXES = CLASES.length;
+
+const gameWrapEl = document.getElementById('game-wrapper');
+const buttonELs = document.getElementsByTagName('button')
+
+
+
+
+
+function generateBoxes(){    
+    for(i=0; i<GRID_COUNT; i++){
+        var div = document.createElement('div');
+        var randomClass = parseInt(Math.random() * NO_OF_BOXES)
+
+        div.classList.add('box')
+        div.classList.add(CLASSES[randomClass])
+
+        gameWrapEl.append(div)
+    }
+}
+
+const mouseDownhandler = function(){
+
+}
+
+function addBoxEventHandlers(){
+    /*
+    swipe right : pageX increase
+    swipe down  : pageY increase
+    
+    swipe up    : pageY decrease
+    swipe left  : pageX decrease
+    */
+    document.addEventListener('mousedown',function(e){
+        console.log(e.pageX, e.pageY);
+    });
+
+    /*gameWrapEl.addEventListener('mousedown' , function(e){
+        console.log('down', e.target);
+        isMouseDown = true;
+    });
+
+    gameWrapEl.addEventListener('mouseup' , function(e){
+        console.log('up', e.target);
+        isMouseDown = false;
+    });
+
+    gameWrapEl.addEventListener('mousemove' , function(e){
+        if(isMouseDown == true){
+            console.log('move', e.target);
+        }
+    })*/
+}
+
+function gameInit(){
+    generateBoxes();
+    addBoxEventHandlers();
+}
+
+
+gameInit();
+
+/*
+function gameWrapEl = document.getElementById('game-wrapper');
+
 const colors = ['lightblue', 'lightred', 'lightgreen', 'lightyellow', 'lightpink'];
+
 var boxes = document.getElementsByClassName('box');
 
 for(var i=0; i<boxes.length; i++){
     boxes[i].style.backgroundColor = colors[parseInt(Math.random()*5)];
 }
+*/
