b63e524354d548576dab0fc9a919e1575301c726
dav-par <david.park86@gmail.com>
2021-01-17 10:25:14 +0000

    colours and numbers, added id code to html

--- Candy/script.js
+++ Candy/script.js
@@ -10,18 +10,19 @@
 4 5 1 2 3 4 5 1
 2 3 4 5 1 2 3 4 */
 
+
+
 const GRID_SIZE = 8;
 const GRID_COUNT = GRID_SIZE*GRID_SIZE;
 
 const CLASES = ['c1', 'c2', 'c3', 'c4', 'c5'];
+
 const NO_OF_BOXES = CLASES.length;
 
 const gameWrapEl = document.getElementById('game-wrapper');
-const buttonELs = document.getElementsByTagName('button')
 
-
-
-
+var isMouseDown = false;
+var boxToSwap, boxToSwapWith;
 
 function generateBoxes(){    
     for(i=0; i<GRID_COUNT; i++){
@@ -29,17 +30,29 @@ function generateBoxes(){
         var randomClass = parseInt(Math.random() * NO_OF_BOXES)
 
         div.classList.add('box')
-        div.classList.add(CLASSES[randomClass])
+        div.classList.add(CLASES[randomClass]);
+
+        div.dataset.id = i;
+        div.innerHTML = i;
+        div.id = 'box'+i;
 
         gameWrapEl.append(div)
     }
 }
 
 const mouseDownhandler = function(){
+}
 
+function getSwipeDirection(pageXstart, pageXEnd, pageYStart, pageXEnd){
+    var deltaX = pageXEnd - pageXstart;
+    var deltaY = pageYEnd - pageYstart;
+
+    console.log('deltaX ', deltaX);
+    console.log('deltaY ', deltaY);
 }
 
 function addBoxEventHandlers(){
+    var pageXStart, pageXEnd, pageYStart, pageYEnd;
     /*
     swipe right : pageX increase
     swipe down  : pageY increase
@@ -47,25 +60,22 @@ function addBoxEventHandlers(){
     swipe up    : pageY decrease
     swipe left  : pageX decrease
     */
-    document.addEventListener('mousedown',function(e){
-        console.log(e.pageX, e.pageY);
-    });
 
-    /*gameWrapEl.addEventListener('mousedown' , function(e){
-        console.log('down', e.target);
-        isMouseDown = true;
+    gameWrapEl.addEventListener('mousedown' , function(e){
+        console.log('down', e.pageX, e.pageY);
+        
+        pageXStart = e.pageX;
+        pageYStart = e.pageY;
     });
 
     gameWrapEl.addEventListener('mouseup' , function(e){
-        console.log('up', e.target);
-        isMouseDown = false;
-    });
+        console.log('up', e.pageX, e.pageY);
+        
+        pageXEnd = e.pageX;
+        pageYEnd = e.pageY;
 
-    gameWrapEl.addEventListener('mousemove' , function(e){
-        if(isMouseDown == true){
-            console.log('move', e.target);
-        }
-    })*/
+        getSwipeDirection(pageXstart, pageXEnd, pageYStart, pageYEnd);
+    });
 }
 
 function gameInit(){
@@ -75,15 +85,3 @@ function gameInit(){
 
 
 gameInit();
-
-/*
-function gameWrapEl = document.getElementById('game-wrapper');
-
-const colors = ['lightblue', 'lightred', 'lightgreen', 'lightyellow', 'lightpink'];
-
-var boxes = document.getElementsByClassName('box');
-
-for(var i=0; i<boxes.length; i++){
-    boxes[i].style.backgroundColor = colors[parseInt(Math.random()*5)];
-}
-*/
